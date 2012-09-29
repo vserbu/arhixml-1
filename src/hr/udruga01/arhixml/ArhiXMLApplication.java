613e4dfaf64b6d536754cd8768b5080add32b869
hrle <hrvoje.varga@gmail.com>
2012-09-29 20:43:11 +0200

    Initial project creation.

---
+++ src/hr/udruga01/arhixml/ArhiXMLApplication.java
@@ -0,0 +1,14 @@
+package hr.udruga01.arhixml;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+
+public class ArhiXMLApplication extends Application {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() {
+		Window mainWindow = new Window("ArhiXML");
+		setMainWindow(mainWindow);
+	}
+}
